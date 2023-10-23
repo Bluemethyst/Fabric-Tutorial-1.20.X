c2a51f7689f40f41a78babad3f4d9c3f8ca33108
Bluemethyst <Bluemethyst@users.noreply.github.com>
2023-10-23 15:35:45 +1300

    Initial commit

---
+++ src/main/java/net/bluemethyst/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.bluemethyst.tutorialmod;
+
+import net.bluemethyst.tutorialmod.block.ModBlocks;
+import net.bluemethyst.tutorialmod.item.ModItemGroups;
+import net.bluemethyst.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID= "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
---
+++ src/main/java/net/bluemethyst/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.bluemethyst.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
